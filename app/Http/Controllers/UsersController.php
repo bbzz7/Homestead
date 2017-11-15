0bc2cb2ea88e2ed01b529c14fcfdd1dcfe9596b6
bbzz7 <bbzz7@163.com>
2017-11-15 22:40:56 +0800

    Finish users list

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,9 +10,15 @@ class UsersController extends Controller
 {
     //
     public function __construct() {
-        $this->middleware('auth',[
-           'except'=>['show','create','store']
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index']
         ]);
+    }
+
+    //
+    public function index(User $user) {
+        $users=$user->paginate(10);
+        return view('users.index', compact('users'));
     }
 
     //
@@ -45,25 +51,25 @@ class UsersController extends Controller
 
     //
     public function edit(User $user) {
-        $this->authorize('update',$user);
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     //
     public function update(User $user, Request $request) {
-        $this->validate($request,[
-            'name' => 'required|max:50',
+        $this->validate($request, [
+            'name'     => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
         ]);
 
-        $data=[];
-        $data['name']=$request->name;
-        if($request->password){
-            $data['password']=bcrypt($request->password);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
         }
         $user->update($data);
 
         session()->flash('success', '个人资料更新成功！');
-        return redirect()->route('users.show',$user->id);
+        return redirect()->route('users.show', $user->id);
     }
 }
