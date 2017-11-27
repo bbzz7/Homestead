584368154a37f0887b557045e63ea4c39f8f48fd
bbzz7 <bbzz7@163.com>
2017-11-27 12:06:58 +0800

    Merge branch 'user-crud'
    # Conflicts:
    #	routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,4 @@ Route::post('login', 'SessionController@store')->name('login');
 Route::delete('login', 'SessionController@destroy')->name('logout');
 Route::delete('login8', 'SessionController@destroy')->name('logout8');
 Route::delete('login11', 'SessionController@destroy')->name('logout11');
+Route::delete('login20', 'SessionController@destroy')->name('logout20');
